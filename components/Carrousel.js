c4601d0c711455f682d8b25888a345fdc3365e73
Pedro <pguerrerox@gmail.com>
2020-09-01 01:02:50 -0400

    v0.7.1 - Carousel adjustments

--- components/Carrousel.js
+++ components/Carrousel.js
@@ -3,15 +3,17 @@ export default function Carrousel(props) {
   const data = props.data;
   const isActive = (index) => index === 0 ? "active" : "";
 
-  const bgImage = {
+  const carrouselInner = {
     width: "100%",
-    minHeight: "65vh",
-    backgroundRepeat: "no-repeat",
-    backgroundSize: "cover",
+    maxHeight: "900px",
   }
+  const carrouselItem = {
+    maxHeight: "inherit"
+  }
+
   const carrouselArrows = {
-    width: "5%",
-    background: "#00000040"
+    width: "50px",
+    background: "#00000090"
   }
   const carrouselIndicator = {
     width: "15px",
@@ -21,7 +23,7 @@ export default function Carrousel(props) {
   }
 
   return (
-    <section className='Carrousel'>
+    <section className='Carrousel d-none d-sm-block'>
       <div id="carouselIndicators" className="carousel slide" data-ride="carousel">
         <ol className="carousel-indicators">
           {
@@ -32,16 +34,15 @@ export default function Carrousel(props) {
             })
           }
         </ol>
-
-        <div className="carousel-inner">
+        <div className="carousel-inner" style={carrouselInner}>
           {
             data.map((item, index) => {
               return (
-                <div className={"carousel-item " + isActive(index)} style={{ backgroundImage: `url(/carrousel/${item.image}.jpeg)`, backgroundPosition: item.positionbackground, ...bgImage }} key={index}>
-                  <div className='position-relative' style={{ ...item.positioncaptiontop, ...item.positioncaptionleft }}>
-                    <h3 className="display-1 text-white shadow-effect" style={{ textShadow: "-3px 3px 1px black" }}>{item.caption}</h3>
-                    <h5 className="text.muted text-light">{item.subcaption}</h5>
-
+                <div className={"carousel-item position-relative " + isActive(index)} key={index}>
+                  <img src={`/carrousel/${item.image}.jpg`} class="d-block w-100" alt={item.alt}/>
+                  <div className='position-absolute' style={{ ...item.positioncaptiontop, ...item.positioncaptionleft }}>
+                    <h1 className="text-white shadow-effect" style={{ textShadow: "-2px 2px 1px black" }}>{item.caption}</h1>
+                    <h2 className="text-black">{item.subcaption}</h2>
                   </div>
                 </div>
               )
@@ -66,3 +67,53 @@ export default function Carrousel(props) {
     </section>
   )
 }
+
+// ,
+//       {
+//         "image":"cocina2",
+//         "positionbackground": "center center",
+//         "caption": "Camilla y Vainilla",
+//         "subcaption": "una vaina bacana!",
+//         "positioncaptiontop": {"top": "250px"},
+//         "positioncaptionleft":{"left": "25%"}
+//       },
+//       {
+//         "image":"cocina3",
+//         "positionbackground": "center bottom",
+//         "caption": "Espacio y pisos, sontaillo",
+//         "subcaption": "pisa bien el piso...",
+//         "positioncaptiontop": {"top": "350px"},
+//         "positioncaptionleft":{"left": "20%"}
+//       },
+//       {
+//         "image":"cocina4",
+//         "positionbackground": "center bottom",
+//         "caption": "Espacio y pisos, sontaillo",
+//         "subcaption": "pisa bien el piso...",
+//         "positioncaptiontop": {"top": "350px"},
+//         "positioncaptionleft":{"left": "20%"}
+//       },
+//       {
+//         "image":"cuarto",
+//         "positionbackground": "center bottom",
+//         "caption": "Espacio y pisos, sontaillo",
+//         "subcaption": "pisa bien el piso...",
+//         "positioncaptiontop": {"top": "350px"},
+//         "positioncaptionleft":{"left": "20%"}
+//       },
+//       {
+//         "image":"sala",
+//         "positionbackground": "center bottom",
+//         "caption": "Espacio y pisos, sontaillo",
+//         "subcaption": "pisa bien el piso...",
+//         "positioncaptiontop": {"top": "350px"},
+//         "positioncaptionleft":{"left": "20%"}
+//       },
+//       {
+//         "image":"sotano",
+//         "positionbackground": "center bottom",
+//         "caption": "Espacio y pisos, sontaillo",
+//         "subcaption": "pisa bien el piso...",
+//         "positioncaptiontop": {"top": "350px"},
+//         "positioncaptionleft":{"left": "20%"}
+//       }
